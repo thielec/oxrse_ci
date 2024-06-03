a46349abcba9b6ac66892b04421bc2b1adb6c2dd
Jan Christoph Thiele <63301724+thielec@users.noreply.github.com>
2024-06-03 16:28:50 +0100

    Added test case for mixed inputs

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -17,3 +17,4 @@ def test_minimum():
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
     assert ci_course.minimum("1", "2") is None
+    assert ci_course.minimum(1, "2") == 1
